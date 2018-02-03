7abaa96e1c7ee9e39f640c99a74e301ce36decf7
Jer'Maine Montiel <jermaine.m.jones@outlook.com>
2018-02-03 10:04:21 -0600

    typos ARGH

--- base.js
+++ base.js
@@ -7,13 +7,13 @@ $(document).ready( () => {
     $jammies.replaceWith('<a id="jammies" class="nav-item" href="/jammies"><img src="assets/music-button.png" /></a>');
     $moods.replaceWith('<a id="moods" class="nav-item" href="/moods"><img src="assets/ig-button.png" /></a>');
     $about.replaceWith('<a id="about" class="nav-item" href="javascript:void(0)"><img src="assets/about-button.png" /></a>');
-    $type.replaceWith('<img class="gif" src="assets/mobile.png" />');
+    $type.replaceWith('<img class="type" src="assets/mobile.png" />');
    }
   else {
     $jammies.replaceWith('<a id="jammies" class="nav-item" href="/jammies">JAMMIES</a>');
     $moods.replaceWith('<a id="moods" class="nav-item" href="/moods">MOODS</a>');
     $about.replaceWith('<a id="about" class="nav-item" href="javascript:void(0)">ABOUT</a>');
-    $type.replaceWith('<img class="gif" src="assets/main.png" />');
+    $type.replaceWith('<img class="type" src="assets/main.png" />');
   }
 
   $("a#about").click ( () => {
