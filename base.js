c9ee4ff13ff30428643976fcff18ca610fe30ec6
Jer'Maine Montiel <jermaine.m.jones@outlook.com>
2018-02-03 10:03:29 -0600

    code

---
+++ base.js
@@ -0,0 +1,22 @@
+$(document).ready( () => {
+
+  let $jammies = $('a#jammies'), $moods = $('a#moods'), $about = $('a#about'), $type = $('img.type'), $text = $('div.text');
+
+  const mq = window.matchMedia( "(max-width: 414px)");
+  if (mq.matches) {
+    $jammies.replaceWith('<a id="jammies" class="nav-item" href="/jammies"><img src="assets/music-button.png" /></a>');
+    $moods.replaceWith('<a id="moods" class="nav-item" href="/moods"><img src="assets/ig-button.png" /></a>');
+    $about.replaceWith('<a id="about" class="nav-item" href="javascript:void(0)"><img src="assets/about-button.png" /></a>');
+    $type.replaceWith('<img class="gif" src="assets/mobile.png" />');
+   }
+  else {
+    $jammies.replaceWith('<a id="jammies" class="nav-item" href="/jammies">JAMMIES</a>');
+    $moods.replaceWith('<a id="moods" class="nav-item" href="/moods">MOODS</a>');
+    $about.replaceWith('<a id="about" class="nav-item" href="javascript:void(0)">ABOUT</a>');
+    $type.replaceWith('<img class="gif" src="assets/main.png" />');
+  }
+
+  $("a#about").click ( () => {
+      $text.toggleClass('hide');
+  });
+});
---
+++ jammies/jammies.js
@@ -0,0 +1,1 @@
+window.location = "https://www.mixcloud.com/smnphx";
