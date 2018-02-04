034eef027ab3525a5fd27a89ddcfa49cdcaed923
Jer'Maine Montiel <jermaine.m.jones@outlook.com>
2018-02-03 18:45:31 -0600

    the changes are here

--- base.js
+++ base.js
@@ -1,22 +1,21 @@
 $(document).ready( () => {
-
-  let $jammies = $('a#jammies'), $moods = $('a#moods'), $about = $('a#about'), $type = $('img.type'), $text = $('div.text');
-
-  const mq = window.matchMedia( "(max-width: 414px)");
-  if (mq.matches) {
-    $jammies.replaceWith('<a id="jammies" class="nav-item" href="/jammies"><img src="assets/music-button.png" /></a>');
-    $moods.replaceWith('<a id="moods" class="nav-item" href="/moods"><img src="assets/ig-button.png" /></a>');
-    $about.replaceWith('<a id="about" class="nav-item" href="javascript:void(0)"><img src="assets/about-button.png" /></a>');
-    $type.replaceWith('<img class="type" src="assets/mobile.png" />');
-   }
-  else {
-    $jammies.replaceWith('<a id="jammies" class="nav-item" href="/jammies">JAMMIES</a>');
-    $moods.replaceWith('<a id="moods" class="nav-item" href="/moods">MOODS</a>');
-    $about.replaceWith('<a id="about" class="nav-item" href="javascript:void(0)">ABOUT</a>');
-    $type.replaceWith('<img class="type" src="assets/main.png" />');
-  }
-
-  $("a#about").click ( () => {
-      $text.toggleClass('hide');
+  let $jammies = $('a#jammies'), $moods = $('a#moods'), $about = $('a#about');
+  $about.click( () => {
+    let $gif = $('img.img-fluid'), $text = $('div.text');
+    $gif.toggleClass("hide");
+    $text.toggleClass("hide");
   });
+
+  const mq = window.matchMedia( "(max-width: 414px)" );
+  if(mq.matches) {
+    $jammies.replaceWith('<a id="jammies" class="nav-item" href="/jammies"><img id="button" src="assets/music-button.png" /></a>');
+    $moods.replaceWith('<a id="moods" class="nav-item" href="/moods"><img id="button" src="assets/ig-button.png" /></a>');
+    $about.replaceWith('<a id="about" class="nav-item" href="javascript:void(0)"><img id="button" src="assets/about-button.png" /></a>');
+    }
+    else {
+      $jammies.replaceWith('<a id="jammies" class="nav-item tk-league-gothic" href="/jammies">jammies</a>');
+      $moods.replaceWith('<a id="moods" class="nav-item tk-league-gothic" href="/moods">moods</a>');
+      $about.replaceWith('<a id="about" class="nav-item tk-league-gothic" href="javascript:void(0)">about</a>');
+    }
+
 });
