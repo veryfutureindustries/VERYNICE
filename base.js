dc2c9b6cafcd8e78dd73fc0e2ed3c5211791902f
Jer'Maine Montiel <jermaine.m.jones@outlook.com>
2018-02-03 21:15:15 -0600

    fixes

--- base.js
+++ base.js
@@ -1,20 +1,6 @@
-$(document).ready( function() {
+function toggle() {
+  let $text = document.getElementById("text"), $gif = document.getElementById("gif");
+  $text.toggleClass("hide");
+  $gif.toggleClass("hide");
 
-  $('#about').click( function() {
-    $('#gif').toggleClass("hide", true);
-    $('#text').toggleClass("hide", false);
-  });
-
-  const mq = window.matchMedia( "(max-width: 414px)" );
-  if(mq.matches) {
-    $('#jammies').replaceWith('<a id="jammies" class="nav-item" href="/jammies"><img id="button" src="assets/music-button.png" /></a>');
-    $('#moods').replaceWith('<a id="moods" class="nav-item" href="/moods"><img id="button" src="assets/ig-button.png" /></a>');
-    $('#about').replaceWith('<a id="about" class="nav-item" href="javascript:void(0)"><img id="button" src="assets/about-button.png" /></a>');
-    }
-    else {
-      $('#jammies').replaceWith('<a id="jammies" class="nav-item tk-league-gothic" href="/jammies">jammies</a>');
-      $('#moods').replaceWith('<a id="moods" class="nav-item tk-league-gothic" href="/moods">moods</a>');
-      $('#about').replaceWith('<a id="about" class="nav-item tk-league-gothic" href="javascript:void(0)">about</a>');
-    }
-
-});
+}
